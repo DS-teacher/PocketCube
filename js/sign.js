8282bb09ae7b5a4c79dd529e83702fad275cecf4
dongty1 <dongty1@yonyou.com>
2021-06-02 17:25:17 +0800

    登录页

---
+++ js/sign.js
@@ -0,0 +1,6 @@
+document.querySelector(".move").addEventListener("click", function () {
+  document.querySelector(".content").classList.toggle("s--signup");
+});
+document.querySelector(".movetoo").addEventListener("click", function () {
+  document.querySelector(".content").classList.toggle("s--signup");
+});
